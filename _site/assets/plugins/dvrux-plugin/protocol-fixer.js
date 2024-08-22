07ed95e05da9f1b3723fafb7dbc67c0888dcca45
Yarianna Tineo <47702016+yariannatineo@users.noreply.github.com>
2024-08-22 14:53:23 -0600

    assets

--- _site/assets/plugins/dvrux-plugin/protocol-fixer.js
+++
@@ -1,9 +0,0 @@
-videojs.plugin('protocolFixer', function() {
-    var player = this,
-        protocol = window.location.protocol;
-    player.on('loadedmetadata', function() {
-        var source = player.src(),
-            srcWithoutProcol = source.substring(source.indexOf('//'));
-        player.src(protocol + srcWithoutProcol);
-    });
-});
